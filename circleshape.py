1647f0602b5650dd31a7df86d41246772cb31546
Kopisky <tomasino.kop@gmail.com>
2025-08-27 15:35:01 +0200

    ability to move + collision

--- circleshape.py
+++ circleshape.py
@@ -1,6 +1,5 @@
 import pygame
 
-# Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
         if hasattr(self, "containers"):
@@ -20,3 +19,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def distance(self, other):
+         return self.position.distance_to(other.position) <= self.radius + other.radius
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from constants import *
 from player import Player
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
@@ -9,7 +12,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
@@ -20,6 +29,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.distance(player):
+                print("Game Over!")
+                sys.exit()
+
 
         screen.fill("black")
 
